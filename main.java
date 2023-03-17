9b71158615a40ea945b5dd2633739779c1fb6bfe
Ramil <ramilkurbanov666@gmail.com>
2023-03-17 14:41:42 +0300

    calc

---
+++ main.java
@@ -0,0 +1,20 @@
+public class main {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static double divide(double a, double b) {
+        if (b == 0) {
+            throw new ArithmeticException("Division by zero!");
+        }
+        return a / b;
+    }
+}
